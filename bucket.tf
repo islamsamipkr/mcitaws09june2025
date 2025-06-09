ce8deaa6189ade3a7f5b887986ea32c86963371d
Sami Islam <islam.samipkr@gmail.com>
2025-06-09 17:57:25 -0400

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -1,7 +1,14 @@
+
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "sami-simple-bucket-123456789" # must be globally unique
+  bucket = "my-modern-secure-bucket-123456789"
+  force_destroy = true
 }
 
-resource "aws_s3_bucket_acl" "my_bucket_acl" {
+resource "aws_s3_bucket_public_access_block" "block_public" {
   bucket = aws_s3_bucket.my_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
