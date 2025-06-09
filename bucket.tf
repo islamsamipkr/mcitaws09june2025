47f7e11f075bb1bb98677bedc67df01066c7e5b5
Sami Islam <islam.samipkr@gmail.com>
2025-06-09 17:48:49 -0400

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -1,4 +1,8 @@
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-simple-bucket-123456789" # Must be globally unique
+  bucket = "my-simple-bucket-123456789" # must be globally unique
+}
+
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.id
   acl    = "private"
 }
