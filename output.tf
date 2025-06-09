e5f13fa9b9c14f580428fab594a1cb055710de4f
Sami Islam <islam.samipkr@gmail.com>
2025-06-09 18:37:31 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "bucket_name" {
   value = aws_s3_bucket.my_bucket.bucket
 }
+output "private_key_pem" {
+  value     = tls_private_key.ec2_key.private_key_pem
+  sensitive = true
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+}
