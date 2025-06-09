14594f5f94bdfc679db8e82f00004c7438110367
Sami Islam <islam.samipkr@gmail.com>
2025-06-09 18:32:08 -0400

    Create ec2instance.tf

---
+++ ec2instance.tf
@@ -0,0 +1,41 @@
+resource "aws_key_pair" "deployer" {
+  key_name   = "my-key"  # Change this to your actual AWS key pair name
+  public_key = file("~/.ssh/id_rsa.pub")  # Adjust if your key is elsewhere
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # SSH
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # HTTP
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-0c7217cdde317cfec" # Amazon Linux 2023 AMI for us-east-1
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "ModernEC2"
+  }
+}
