7ec5aa21e7cca893e36e9fd89e9eb6dca09b63da
Sami Islam <islam.samipkr@gmail.com>
2025-06-09 20:16:04 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -10,7 +10,7 @@ output "public_ip" {
   value = aws_instance.mcitsami.public_ip
 }
 output "public_ips" {
-  description = "Public IPs of EC2 instances"
+  description = "Public IPs of all EC2 instances"
   value = {
     for name, instance in aws_instance.mcitsami :
     name => instance.public_ip
