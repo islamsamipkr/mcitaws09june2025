687501bc7fdeabd50c869f8844fc095b6c3ea44e
Sami Islam <islam.samipkr@gmail.com>
2025-06-09 20:06:54 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,3 +4,12 @@ variable "AWS_ACCESS_KEY"{
 variable "AWS_CLIENT_SECRET"{
   type=string
 }
+variable "movie_names" {
+  default = [
+    "Inception",
+    "Interstellar",
+    "The_Dark_Knight",
+    "Tenet",
+    "Dunkirk"
+  ]
+}
